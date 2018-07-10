ba27ab1fa16283a04a4ef98e4829c4073638887a
Charles Stover <charles.stover@walmart.com>
2018-07-10 15:03:23 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # react-multi-context
 Manage multiple contexts with a single React component.
+
+## Install
+`npm install react-multi-context --save` or `yarn add react-multi-context`
+
+## Use
+Create the context by importing and executing `createMultiContext` wherever you want to create context.
+Then, import that multi-context instance as needed.
+
+Use the `set` prop to set a context's value.
+Changing the value on a key-value pair in a context will cause all getters for that key to re-render.
+
+Use the `get` prop to get a context's value.
+Using this prop will execute the children function by passing the corresponding values of the context as the parameters.
+
+## Example
+```JS
+// Parent.js
+import createMultiContext from 'react-multi-context';
+
+export ParentContext = createMultiContext(); // context to provide to children
+
+class Parent extends React.Component {
+  render() {
+    return (
+      <ParentContext
+        set={{
+          cost: 10,
+          project: {
+            name: 'React Multi-Context',
+            version: 1.0
+          },
+          user: 'Charles'
+        }}
+      >
+        <Child />
+      </ParentContext>
+    );
+  }
+}
+
+// Child.js
+import { ParentContext } from './Parent'; // context provided from parent
+
+class Child extends React.Component {
+  render() {
+    return (
+      <ParentContext get={[ 'project', 'user' ]}>
+        {(project, user) =>
+          <p>This is a demo of {project.name} v{project.version} by {user}!</p>
+        }
+      </ParentContext>
+    );
+  }
+}
+```
+
+## Example (Shorter)
+``JS
+// parent - writes A and B
+const Parent = () =>
+  <MultiContextInstance set={{ a: 1, b: 2, c: 3 }}>
+    <Child1 />
+    <Child2 />
+  </MultiContextInstance>;
+
+// child1 - reads A
+// Note: Each value is its own context, which is what makes this MULTI-context.
+const Child1 = () =>
+  <MultiContextInstance get={[ 'a' ]}>
+    {(a) => `The value of A is ${a}!`}
+  </MultiContextInstance>;
+
+// child2 - reads A and B
+// Note: Reading multiple values will trigger a re-render if any one read value changes.
+const Child2 = () =>
+  <MultiContextInstance get={[ 'a', 'b' ]}>
+    {(a, b) => `The value of A+B is ${a + b}!`}
+  </MultiContextInstance>;
+
+// child3 - reads B and A
+// Note: The order of the get property corresponds to the order of the function parameters.
+const Child3 = () =>
+  <MultiContextInstance get={[ 'b', 'a' ]}>
+    {(b, a) => `The value of A+B is ${a + b}!`}
+  </MultiContextInstance>;
+``
+
+## Default Values
+You may pass an object of default values for the contexts via the `default` prop.
---
+++ index.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+const providerSort = (a, b) =>
+  a[0] < b[0] ?
+    -1 :
+    1;
+
+const createMultiContext = () => {
+  const contexts = Object.create(null);
+  return class MultiContext extends React.PureComponent {
+
+    createContext(key) {
+      if (!Object.prototype.hasOwnProperty.call(contexts, key)) {
+        contexts[key] = React.createContext(
+          typeof this.props.default === 'object' &&
+          this.props.default !== null &&
+          Object.prototype.hasOwnProperty.call(this.props.default, key) ?
+            this.props.default[key] :
+            null
+        );
+      }
+      return contexts[key];
+    }
+
+    // Recursively generate <Context.Consumer>, with each consumer being a child of the last.
+    getConsumer(children, index, values) {
+
+      // If we have all the consumers, we can spread the values to the child render prop.
+      if (this.props.get.length === index) {
+        return children(...values);
+      }
+
+      // Check if this context exists.
+      const contextKey = this.props.get[index];
+      if (!Object.prototype.hasOwnProperty.call(contexts, contextKey)) {
+        throw new Error('Context `' + contextKey + '` does not exist.');
+      }
+
+      // Create this context's consumer.
+      const Context = contexts[contextKey];
+      return (
+        <Context.Consumer
+          children={
+            (value) =>
+              this.getConsumer(
+                children,
+                index + 1,
+                values.concat([ value ])
+              )
+          }
+        />
+      );
+    }
+
+    getProvider(children, providers, index) {
+
+      // If we have all the consumers, we can spread the values to the child render prop.
+      if (providers.length === index) {
+        return children;
+      }
+
+      // Create the context (if it doesn't exist).
+      const Context = this.createContext(providers[index][0]);
+
+      // Create this context's provider.
+      return (
+        <Context.Provider
+          children={this.getProvider(children, providers, index + 1)}
+          value={providers[index][1]}
+        />
+      );
+    }
+
+    get hasConsumers() {
+      return (
+        Array.isArray(this.props.get) &&
+        this.props.get.length > 0
+      );
+    }
+
+    get hasProviders() {
+      return (
+        typeof this.props.set === 'object' &&
+        this.props.set !== null
+      );
+    }
+
+    renderConsumers(children) {
+
+      // If there are no consumers to get, just return the children.
+      if (!this.hasConsumers) {
+        return children;
+      }
+
+      // If there are consumers to get, get them one at a time.
+      if (typeof children !== 'function') {
+        throw new Error('Consuming context requires a function child.');
+      }
+
+      return this.getConsumer(children, 0, []);
+    }
+
+    renderProviders(children) {
+
+      // If there are no providers to set, just return the children.
+      if (!this.hasProviders) {
+        return children;
+      }
+
+      // If there are providers to set,
+      const providers = Object.entries(this.props.set);
+      if (providers.length === 0) {
+        return children;
+      }
+
+      // Sort them to insure same hierarchy and prevent unnecessary re-rendering (if hierarchy were to change).
+      providers.sort(providerSort);
+
+      // If there are consumers, return a function render prop that sets each provider one at a time.
+      if (this.hasConsumers) {
+        return (...values) => this.getProvider(children(...values), providers, 0);
+      }
+
+      // If there are no consumers, set each provider one at a time.
+      return this.getProvider(children, providers, 0);
+    }
+
+    render() {
+      return this.renderConsumers(
+        this.renderProviders(
+          this.props.children
+        )
+      );
+    }
+  };
+};
+
+export default createMultiContext;

0e179673c247463b4f9761bd5792f0ecaed7514e
Charles Stover <charles.stover@walmart.com>
2018-07-16 15:11:35 -0500

    library in npm build

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
+    library: 'react-multi-context',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'build'),
     umdNamedDefine: true

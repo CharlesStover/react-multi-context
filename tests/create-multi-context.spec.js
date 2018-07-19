50dc6d15718041c402e53f0b41347ee1178029bd
Charles Stover <charles.stover@walmart.com>
2018-07-19 13:56:58 -0500

    basci tests

--- tests/create-multi-context.spec.js
+++ tests/create-multi-context.spec.js
@@ -1,4 +1,4 @@
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { render, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -15,5 +15,69 @@ describe('createMultiContext', () => {
 
   it('should render without crashing', () => {
     shallow(<MultiContext />);
+  });
+
+  it.skip('should not render contexts without getters/setters', () => {
+    const multiContext = shallow(<MultiContext />);
+    expect(multiContext.find('Context').length).toBe(0);
+  });
+
+
+
+  // Set
+  describe('set', () => {
+
+    it('should accept an object', () => {
+      expect(() => {
+        shallow(<MultiContext set={{ a: 1 }} />);
+      }).not.toThrowError();
+    });
+
+    it.skip('should require an object', () => {
+      expect(() => {
+      }).toThrowError();
+    });
+  });
+
+
+
+  // Get
+  describe('get', () => {
+
+    it('should accept a render prop', () => {
+      expect(() => {
+        shallow(<MultiContext set={{ a: 1 }} />);
+        shallow(
+          <MultiContext
+            children={() => null}
+            get={[ 'a' ]}
+          />
+        );
+      }).not.toThrowError();
+    });
+
+    it('should require a render prop', () => {
+      expect(() => {
+        shallow(<MultiContext set={{ a: 1 }} />);
+        shallow(
+          <MultiContext
+            children="children"
+            get={[ 'a' ]}
+          />
+        );
+      }).toThrowError();
+    });
+
+    it.skip('should execute the render prop', () => {
+    });
+  });
+
+
+
+  // Set + Get
+  describe('set + get', () => {
+
+    it.skip('should not get what was just set', () => {
+    });
   });
 });
